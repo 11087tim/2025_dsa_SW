891cb4651e113c279a75d92955f4298c518171e5
bbwinner <bbcam7392@gmail.com>
2025-05-03 21:50:56 +0800

    finish print_grid

---
+++ FE/grid.hpp
@@ -0,0 +1,26 @@
+# ifndef GRID
+# define GRID
+# include <iostream>
+# include <vector>
+# include <unistd.h>
+
+using namespace std;
+
+
+// Consider a 0-based array:
+//  If a queen is placed at i-th row, j-th col, set arr[i] = j. 
+//  Otherwise, set arr[i] = -1
+
+void print_grid(int n, vector<int> &v){
+    system("clear");
+    for(int i=0; i<n; ++i){
+        for(int j=0; j<n; ++j){
+            if((i+j)%2) cout<<"\033[90;7m";
+            else cout<<"\033[95;7m";
+            if(v[i] == j) cout << " Q ";
+            else cout << "   ";
+        }
+        cout << "\n";
+    }
+}
+# endif
