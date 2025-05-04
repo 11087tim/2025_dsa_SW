f559d74c205acd4acb5f7e7a95e6044e9a91661b
bbwinner <bbcam7392@gmail.com>
2025-05-04 21:56:04 +0800

    print_grid

---
+++ src/nqueens.cpp
@@ -0,0 +1,30 @@
+# include <iostream>
+# include <vector>
+# include <unistd.h>
+# include <tabulate/table.hpp>
+
+using namespace std;
+using namespace tabulate;
+using Row_t = Table::Row_t;
+
+void print_grid(int n, vector<int> &v){
+    system("clear");
+
+    Table grid;
+    for(int i=0; i<n; ++i){
+        Row_t tmp;
+        for(int j=0; j<n; ++j){
+            if(v[i] == j) tmp.push_back("Q");
+            else tmp.push_back(" ");
+        }
+        grid.add_row(tmp);
+    }
+    cout << grid << endl;
+
+    sleep(1);
+}
+
+int main()
+{
+    return 0;
+}
